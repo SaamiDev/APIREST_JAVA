88e1bcaf0bf2217ae3b104b46bf9f01886fcd389
SaamiDev <saamideev@gmail.com>
2024-03-18 22:02:21 +0100

    Initial commit: Add project files

---
+++ api_management/src/main/java/com/apirest/api_management/ApiManagementApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.api_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagementApplication.class, args);
+	}
+
+}
